9a1a80a19e6a70557e5ce05edbd99c6ee9aee031
Cris-T14N0 <cris@DESKTOP-K1N56R5>
2023-12-06 17:54:24 +0000

    primeira tarefa

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Contact;
+use App\Models\User;
+use Illuminate\Http\Request;
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
+
+class ContactController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $users = User::all();
+        return view('contacts.index', compact('users'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $userId = auth()->id();
+        # Validação dos dados
+        $validated = $request->validate([
+            'phone' => 'required|string|max:255',
+            'address' => 'required|string|max:255',
+            
+        ]);
+
+        
+        $validated += [
+            'user_id' => $userId,
+        ];
+
+
+        # Gravar na BD 2
+        $contact = new Contact();
+        $contact->create($validated);
+
+        # Feedback and redirect
+        session()->flash('sucesso','Contacto criado!');
+        return redirect(route('contacts.index'));
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //                     
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ resources/views/contacts/index.blade.php
@@ -0,0 +1,99 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Gestor de Contactos') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <div class="overflow-hidden bg-white shadow-sm dark:bg-gray-800 sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <section>
+
+                        <header>
+                            <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
+                                {{ __('Insira dos dados do novo contacto para o criar.') }}
+                            </h2>
+                        </header>
+
+                        <form method="post" action="{{ route('contacts.store') }}" class="mt-6 space-y-6">
+
+                            @csrf
+
+                            <div>
+                                <x-input-label for="phone" :value="__('Contacto (Número de telefone)')" />
+                                <x-text-input id="phone" name="phone" type="text" class="block w-full mt-1" :value="old('phone')" required autofocus autocomplete="nome" />
+                                <x-input-error class="mt-2" :messages="$errors->get('phone')" />
+                            </div>
+
+                            <div>
+                                <x-input-label for="address" :value="__('Endereço')" />
+                                <x-text-input id="address" name="address" type="text" class="block w-full mt-1" :value="old('address')" required autofocus autocomplete="nome" />
+                                <x-input-error class="mt-2" :messages="$errors->get('address')" />
+                            </div>
+
+                            <div class="flex items-center gap-4">
+                                <x-primary-button>{{ __('Adicionar') }}</x-primary-button>
+								@if (session('sucesso'))
+                                    <p
+                                      x-data="{ show: true }"
+                                      x-show="show"
+                                      x-transition
+                                      x-init="setTimeout(() => show = false, 2000)"
+                                      class="text-sm text-gray-600 dark:text-gray-400"
+                                    >{{ Session::get('sucesso') }}</p>
+                                @endif
+                            </div>
+                        </form>
+                        
+                    </section>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <table class="mx-auto max-w-7xl sm:px-6 lg:px-8 w-full bg-white border border-spacing-2 border-slate-500 shadow-md">
+        <thead>
+            <tr class="bg-gray-800 text-white">
+                <th class="px-4 py-2">#</th>
+                <th class="px-4 py-2">Nome</th>
+                <th class="px-4 py-2">Email</th>
+                <th class="px-4 py-2">Telefone</th>
+                <th class="px-4 py-2">Morada</th>
+            </tr>
+        </thead>
+        <tbody>
+            @forelse ($users as $user)
+                <tr class="bg-gray-800 text-white">
+                    <td class="px-4 py-2 border border-slate-700">{{ $user->id }}</td>
+                    <td class="px-4 py-2 border border-slate-700">{{ $user->name }}</td>
+                    <td class="px-4 py-2 border border-slate-700">{{ $user->email }}</td>
+                    
+                    <td class="px-4 py-2 border border-slate-700">
+                        @if ($user->contact)
+                            {{ $user->contact->phone ?: 'N/A' }}
+                        @else
+                            N/A
+                        @endif
+                    </td>
+
+                    <td class="px-4 py-2 border border-slate-700">
+                        
+                        @if ($user->contact)
+                            {{ $user->contact->address ?: 'N/A' }}
+                        @else
+                            N/A
+                        @endif
+                    </td>
+
+                </tr>
+            @empty
+                <tr>
+                    <td colspan="5" class="px-4 py-2 text-center text-gray-500">Ainda sem contactos!</td>
+                </tr>
+            @endforelse
+        </tbody>
+    </table>
+    
+</x-app-layout>
